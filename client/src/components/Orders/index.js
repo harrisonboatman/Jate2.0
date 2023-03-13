b0d6b89ba26cd94ea2776f3a75b09acb8e55e426
Medranomiler1 <medranomiler@gmail.com>
2023-03-13 10:40:26 -0500

    added an employee page, needs code to render content

---
+++ client/src/components/Orders/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import spinner from '../../assets/spinner.gif';
+
+function Orders(){
+
+    return(
+        <>
+        <h1>Order Management Page</h1>
+        </>
+    )
+}
+
+export default Orders;
---
+++ client/src/pages/OrderManagement.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Orders from "../components/Orders"
+
+const OrderManagement = () => {
+    return (
+        <>
+        <Orders />
+        </>
+    )
+}
+
+export default OrderManagement;
