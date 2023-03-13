e3c4957c645d1e0bcf9cc750657b3b7eb90b1d43
Harrison Boatman <hrry.boatman2@gmail.com>
2023-03-13 12:14:01 -0500

    added functionality to ordermanagement page

--- client/src/pages/OrderManagement.js
+++ client/src/pages/OrderManagement.js
@@ -1,10 +1,33 @@
 import React from "react";
 import Orders from "../components/Orders"
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
 
-const OrderManagement = () => {
+function OrderManagement() {
+  const { data } = useQuery(QUERY_USER);
+  let user;
+  let role;
+  let manager;
+
+  if (data) {
+    user = data.user;
+    role = data.user.userType
+
+  }
+  if (role === 'customer') {
+    manager = false
+  } else {
+    manager = true
+}
+    
     return (
         <>
         <Orders />
+        <p>welcome to the page</p>
+
+        {user ? (<p>welcome {user.firstName} who is a {role}</p>): null}
+        {manager ? (<p>yous a manager</p>): null}
+        
         </>
     )
 }
