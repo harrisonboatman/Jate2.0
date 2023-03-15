d71fae6cfed3dc37f5a8c6ebd7081ac938261a6a
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-03-15 02:44:28 -0500

    profile cards completed

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -1,7 +1,11 @@
 
 import React from "react";
 import Michael from "../assets/michael.png"
-// import { faBrands } from "@fortawesome/fontawesome-svg-core"
+import Harrison from "../assets/harrison.png"
+import Darren from "../assets/darren.png"
+import Connor from "../assets/connor.png"
+import Rick from "../assets/Rick.png"
+
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
@@ -9,86 +13,95 @@ import { AiOutlineMail } from "react-icons/ai";
 
 function Footer() {
     return (
-        <div className="container mx-auto mb-6">
-            <div className="wrapper">
-                <div className=" grid grid-flow-row grid-cols-5  gap-8">
-                    <div className="max-w-sx bg-gray-700 rounded-2xl ">
-                    <div className="top-color bg-gray-300 h-20 border-none rounded-t-2xl"></div>
+        <div className="mx-auto w-full">
+            <div className="wrapper bg-white p-10 mb-6">
+                <div className=" grid grid-flow-row grid-cols-5 gap-7 mx-6">
+                    <div className="team-cards relative overflow-hidden">
+                    <div className="max-w-sx bg-gradient-to-br from-black to-zinc-600 h-[300px]">
+                    <div className="top-color bg-green-700 h-20 border-none rounded-t-2xl"></div>
                         <div className="flex flex-col items-center pb-20">
-                            <img src={Michael} className="w-24 mb-3 rounded-full mt-[-48px] ring-[5px] ring-gray-300" />
-                            <h1 className="mb-2 text-2xl font-medium text-gray-50">Michael Smith</h1>
-                            <span className="text-sm text-gray-500">Full Stack</span>
+                            <img src={Darren} className="w-24 mb-5 rounded-full mt-[-48px] ring-[5px] ring-green-700" />
+                            <h1 className="mb-2 text-2xl font-medium text-gray-50">Darren Medrano</h1>
+                            <span className="text-lg text-gray-500 mb-2">Full Stack</span>
                             <div className="flex mt-4 space-x-3 md:mt-6">
-                                <div className="inline-flex items-center text-sm font-medium text-center">
-                                <FaGithub className="text-white mx-5" size={30}/>
-                                <FaLinkedin className="text-white mx-5" size={30}/>
-                                <AiOutlineMail className="text-white mx-5" size={30}/>
+                                <div className="social inline-flex items-center text-sm font-medium text-center">
+                                <FaGithub className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                <FaLinkedin className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                <AiOutlineMail className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
                                 </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="team-cards relative overflow-hidden">
+                    <div className="max-w-sx bg-gradient-to-br from-black to-zinc-600 rounded-2xl  h-[300px]">
+                    <div className="top-color bg-green-700 h-20 border-none rounded-t-2xl"></div>
+                        <div className="flex flex-col items-center pb-20">
+                            <img src={Harrison} className="w-24 mb-4 rounded-full mt-[-48px] ring-[5px] ring-green-700" />
+                            <h1 className="mb-2 text-2xl font-medium text-gray-50">Harrison Boatman</h1>
+                            <span className="text-lg text-gray-500 mb-2">Full Stack</span>
+                            <div className="flex mt-4 space-x-3 md:mt-6">
+                                <div className="social inline-flex items-center text-sm font-medium text-center">
+                                <FaGithub className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                <FaLinkedin className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                <AiOutlineMail className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                </div>
+                            </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="max-w-sx bg-gray-700 rounded-2xl ">
-                    <div className="top-color bg-gray-300 h-20 border-none rounded-t-2xl"></div>
+                    <div className="team-cards relative overflow-hidden">
+                    <div className="max-w-sx bg-gradient-to-br from-black to-zinc-600 rounded-2xl  h-[300px]">
+                    <div className="top-color bg-green-700 h-20 border-none rounded-t-2xl"></div>
                         <div className="flex flex-col items-center pb-20">
-                            <img src={Michael} className="w-24 mb-3 rounded-full mt-[-48px] ring-[5px] ring-gray-300" />
-                            <h1 className="mb-2 text-2xl font-medium text-gray-50">Michael Smith</h1>
-                            <span className="text-sm text-gray-500">Full Stack</span>
+                            <img src={Connor} className="w-24 mb-2 rounded-full mt-[-48px] ring-[5px] ring-green-700" />
+                            <h1 className="mb-2 text-2xl font-medium text-gray-50">Connor Bodin</h1>
+                            <span className="text-lg text-gray-500 mb-2">Full Stack</span>
                             <div className="flex mt-4 space-x-3 md:mt-6">
-                                <div className="inline-flex items-center text-sm font-medium text-center">
-                                <FaGithub className="text-white mx-5" size={30}/>
-                                <FaLinkedin className="text-white mx-5" size={30}/>
-                                <AiOutlineMail className="text-white mx-5" size={30}/>
+                                <div className="social inline-flex items-center text-sm font-medium text-center">
+                                <FaGithub className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                <FaLinkedin className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                <AiOutlineMail className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
                                 </div>
+                            </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="max-w-sx bg-gray-700 rounded-2xl ">
-                    <div className="top-color bg-gray-300 h-20 border-none rounded-t-2xl"></div>
+                    <div className="team-cards relative overflow-hidden">
+                    <div className="max-w-sx bg-gradient-to-br from-black to-zinc-600 rounded-2xl h-[300px]">
+                    <div className="top-color bg-green-700 h-20 border-none rounded-t-2xl"></div>
                         <div className="flex flex-col items-center pb-20">
-                            <img src={Michael} className="w-24 mb-3 rounded-full mt-[-48px] ring-[5px] ring-gray-300" />
+                            <img src={Michael} className="w-24 mb-3 rounded-full mt-[-48px] ring-[5px] ring-green-700" />
                             <h1 className="mb-2 text-2xl font-medium text-gray-50">Michael Smith</h1>
-                            <span className="text-sm text-gray-500">Full Stack</span>
+                            <span className="text-lg text-gray-500 mb-2">Full Stack</span>
                             <div className="flex mt-4 space-x-3 md:mt-6">
-                                <div className="inline-flex items-center text-sm font-medium text-center">
-                                <FaGithub className="text-white mx-5" size={30}/>
-                                <FaLinkedin className="text-white mx-5" size={30}/>
-                                <AiOutlineMail className="text-white mx-5" size={30}/>
+                                <div className="social inline-flex items-center text-sm font-medium text-center">
+                                <FaGithub className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                <FaLinkedin className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                <AiOutlineMail className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
                                 </div>
+                            </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="max-w-sx bg-gray-700 rounded-2xl ">
-                    <div className="top-color bg-gray-300 h-20 border-none rounded-t-2xl"></div>
+                    <div className="team-cards relative overflow-hidden">
+                    <div className="max-w-sx bg-gradient-to-br from-black to-zinc-600 rounded-2xl h-[300px]">
+                    <div className="top-color bg-green-700 h-20 border-none rounded-t-2xl"></div>
                         <div className="flex flex-col items-center pb-20">
-                            <img src={Michael} className="w-24 mb-3 rounded-full mt-[-48px] ring-[5px] ring-gray-300" />
-                            <h1 className="mb-2 text-2xl font-medium text-gray-50">Michael Smith</h1>
-                            <span className="text-sm text-gray-500">Full Stack</span>
-                            <div className="flex mt-4 space-x-3 md:mt-6">
-                                <div className="inline-flex items-center text-sm font-medium text-center">
-                                <FaGithub className="text-white mx-5" size={30}/>
-                                <FaLinkedin className="text-white mx-5" size={30}/>
-                                <AiOutlineMail className="text-white mx-5" size={30}/>
+                            <img src={Rick} className="w-24 mb-4 rounded-full mt-[-48px] ring-[5px] ring-green-700" />
+                            <h1 className="mb-2 text-2xl font-medium text-gray-50">Rick Martinez</h1>
+                            <span className="text-lg text-gray-500 mb-2">Full Stack</span>
+                            <div className=" mt-4 space-x-3 md:mt-6">
+                                <div className="social inline-flex items-center text-sm font-medium text-center">
+                                <FaGithub className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                <FaLinkedin className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
+                                <AiOutlineMail className="text-white mx-5 hover:text-green-500 cursor-pointer duration-200" size={30}/>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div className="max-w-sx bg-gray-700 rounded-2xl ">
-                    <div className="top-color bg-gray-300 h-20 border-none rounded-t-2xl"></div>
-                        <div className="flex flex-col items-center pb-20">
-                            <img src={Michael} className="w-24 mb-3 rounded-full mt-[-48px] ring-[5px] ring-gray-300" />
-                            <h1 className="mb-2 text-2xl font-medium text-gray-50">Michael Smith</h1>
-                            <span className="text-sm text-gray-500">Full Stack</span>
-                            <div className="flex mt-4 space-x-3 md:mt-6">
-                                <div className="inline-flex items-center text-sm font-medium text-center">
-                                <FaGithub className="text-white mx-5" size={30}/>
-                                <FaLinkedin className="text-white mx-5" size={30}/>
-                                <AiOutlineMail className="text-white mx-5" size={30}/>
-                                </div>
-                            </div>
                         </div>
                     </div>
                     
