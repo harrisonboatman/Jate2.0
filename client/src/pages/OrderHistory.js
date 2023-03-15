a2f0fc79419072e2925ec5a0be1909090d6bd528
Harrison Boatman <hrry.boatman2@gmail.com>
2023-03-15 10:24:37 -0500

    pull commit

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -5,12 +5,15 @@ import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
 function OrderHistory() {
-  const { data } = useQuery(QUERY_USER);
+  const { data, error, isLoading } = useQuery(QUERY_USER);
   let user;
+  
 
   if (data) {
     user = data.user;
   }
+console.log(user)
+console.log(data)
 
   return (
     <>
--- client/src/pages/OrderManagement.js
+++ client/src/pages/OrderManagement.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from '@apollo/client';
 import { QUERY_ALL_USERS, QUERY_USER } from '../utils/queries';
 import { useMutation } from "@apollo/client";
@@ -7,15 +7,17 @@ import { ADD_PRODUCT, UPDATE_USER } from "../utils/mutations";
 
 function OrderManagement(props) {
     const { data } = useQuery(QUERY_USER);
-    const thing = useQuery(QUERY_ALL_USERS)
+    const thing =  useQuery(QUERY_ALL_USERS)
     let people;
     let user;
     let role;
     let manager = false;
     let admin = false;
     const [formState, setFormState] = useState({ name: '', description: '', price: 0, category: '', image: '', _id: '', userType: '' });
-    const [product, { error }] = useMutation(ADD_PRODUCT);
-    const [job] = useMutation(UPDATE_USER)
+    const [product] = useMutation(ADD_PRODUCT);
+    const [job] = useMutation(UPDATE_USER);
+   
+    
 
 
     const handleFormSubmit = async (event) => {
@@ -23,7 +25,8 @@ function OrderManagement(props) {
         let productPrice = document.querySelector('#price').value
         productPrice = JSON.parse(productPrice);
         let imageURL = document.querySelector('#image').value
-        console.log(imageURL)
+        console.log(imageURL);
+        
 
         try {
             const mutationResponse = await product({
@@ -54,7 +57,7 @@ function OrderManagement(props) {
         } catch (e) {
             console.log(e);
         }
-        document.location.reload();
+        // document.location.reload();
 
     };
 
@@ -72,14 +75,16 @@ function OrderManagement(props) {
     }
     if (thing) {
         people = thing.data;
+        console.log(thing.loading)
+    
     }
     if (role === 'manager') {
         manager = true
     } else if (role === 'admin') {
+        
         admin = true
     } else {
-        manager = false;
-        admin = false;
+        return (<p>you are not welcome here</p>)
     }
 
     return (
@@ -155,18 +160,17 @@ function OrderManagement(props) {
                 </div>
 
             </div>) : null}
-
             {admin ? (
-
                 <div className='mt-[3rem] mx-4 flex justify-center '>
                     <div class="bg-gray-900 rounded-xl p-10">
                         <h2 class="text-green-500 my-5 text-center font-extrabold">wow you are an admin!</h2>
                         <form onSubmit={handleJobChange}>
                             <label class="text-green-500" for="_id">Choose a user: </label>
                             <select name="_id" id="_id">
-                                {people.users.map((person) => (
-                                    <option value={person._id}>Name: {person.firstName} ID: {person._id}</option>
+                                {thing.data.users.map((person, index) => (
+                                    <option key = {index} value={person._id}>Name: {person.firstName} ID: {person._id}</option>
                                 ))}
+                                
                             </select>
                             <label class='text-green-500'>Choose which job you would like them to be now:</label>
                             <select name='userType' id='userType'>
@@ -185,7 +189,7 @@ function OrderManagement(props) {
                     </div>
                 </div>
             ) : null}
-
+                                
         </>
     )
 }
