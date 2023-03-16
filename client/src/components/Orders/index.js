6c0b038c57daa41bf0a426b4a06f50653c590c7c
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-03-16 01:39:07 -0500

    placeholder cards for Employee orders

--- client/src/components/Orders/index.js
+++ client/src/components/Orders/index.js
@@ -3,9 +3,17 @@ import { Link } from "react-router-dom";
 import ProductItem from '../ProductItem';
 import spinner from '../../assets/spinner.gif';
 import image1 from '../../assets/connor.png'
+import darren from '../../assets/darren.png'
 function Orders(item){
     const { image, name, _id} = item;
 
+
+      {/* <Link to={`/products/${_id}`}>
+                                <img
+                  class=" rounded-lg object-cover ease-in duration-300 hover:scale-105 h-full"
+                  src={image1}
+                />
+                                </Link> */}
 
     return(
         <>
@@ -13,20 +21,16 @@ function Orders(item){
             <div className="mx-1 flex flex-wrap justify-center">
 {/* -------------------------- Submitted Orders ---------------------------------------------------------------*/}
 
-                <div className='w-full'>  
-                    <h2 className="justify-center mt-20 text-2xl font-extrabold text-center">Submitted</h2>
+                <div>  
+                    <h2 className="justify-center mt-20 text-2xl font-extrabold text-center flex">Submitted</h2>
                     <div>
-                        {/* This is where orders that are submitted will display */}
+                       
                         <div class="min-h-fit p-10 md:w-3/4 flex justify-center items-center scale-75 hover:scale-80 hover:ease-in-out " >
                             <div class="container mx-auto p-9 bg-white max-w-sm rounded-2xl overflow-hidden shadow-md hover:shadow-2xl transition duration-300">
-                                {/* <Link to={`/products/${_id}`}>
-                                <img
-                  class=" rounded-lg object-cover ease-in duration-300 hover:scale-105 h-full"
-                  src={image1}
-                />
-                                </Link> */}
+                               
                                 <div class="flex flex-col items-center">
                                     <div>
+                                        <img src={darren} />
                                         <h1 class="mt-5 text-2xl font-semibold">Status: Submitted</h1>
                                         <p>Order: Bacon and Egg Taco & Migas & Mojito </p>
                                         <p></p>
@@ -45,26 +49,23 @@ function Orders(item){
 {/* -------------------------------------------------------------------------------------------------------------*/}
 {/* -------------------------- In Progress Orders ---------------------------------------------------------------*/}
 
-                <div className='w-full'>  
-                    <h2 className="justify-center mt-20 text-2xl font-extrabold text-center">In Progress</h2>
+                <div>  
+                    <h2 className="justify-center mt-20 text-2xl font-extrabold text-center flex">In Progress</h2>
                     <div>
-                        {/* This is where orders that are in progress will display */}
-                        <div class="p-10 md:w-1/4 flex items-center hover:ease-in-out " >
-                            <div class="container mx-auto p-9 bg-white w-full rounded-2xl  shadow-md hover:shadow-2xl transition duration-300">
-                                <Link to={`/products/${_id}`}>
-                                    <img
-                                        class="rounded-xl"
-                                        src={`/assets/${image}`}
-                                        alt={name}
-                                    />
-                                </Link>
-                                <div class="flex justify-between items-center">
+                       
+                        <div class="min-h-fit p-10 md:w-3/4 flex justify-center items-center scale-75 hover:scale-80 hover:ease-in-out " >
+                            <div class="container mx-auto p-9 bg-white max-w-sm rounded-2xl overflow-hidden shadow-md hover:shadow-2xl transition duration-300">
+                               
+                                <div class="flex flex-col items-center">
                                     <div>
-                                        <h1 class="mt-5 text-2xl font-semibold">{name}</h1>
+                                    <img src={darren} />
+                                        <h1 class="mt-5 text-2xl font-semibold">Status: Submitted</h1>
+                                        <p>Order: Bacon and Egg Taco & Migas & Mojito </p>
+                                        <p></p>
                                     </div>
-                                    <div>
+                                    <div className='pt-5'>
                                         <button class="text-white text-md font-semibold bg-green-400 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">
-                                            Move to Completed
+                                            Move to In Progress
                                         </button>
                                     </div>
                                 </div>
@@ -74,26 +75,23 @@ function Orders(item){
                 </div>
 {/* ------------------------------------------------------------------------------------------------------------ */}
 {/* -------------------------- Completed Orders -----------------------------------------------------------------*/}
-                <div className='w-full'>
-                    <h2 className="justify-center mt-20 text-2xl font-extrabold text-center">Completed</h2>
+                <div>  
+                    <h2 className="justify-center mt-20 text-2xl font-extrabold text-center flex">Completed</h2>
                     <div>
-                        {/* This is where orders that are in progress will display */}
-                        <div class="p-10 md:w-1/4 flex items-center hover:ease-in-out " >
-                            <div class="container mx-auto p-9 bg-white w-full rounded-2xl  shadow-md hover:shadow-2xl transition duration-300">
-                                <Link to={`/products/${_id}`}>
-                                    <img
-                                        class="rounded-xl"
-                                        src={`/assets/${image}`}
-                                        alt={name}
-                                    />
-                                </Link>
-                                <div class="flex justify-between items-center">
+                       
+                        <div class="min-h-fit p-10 md:w-3/4 flex justify-center items-center scale-75 hover:scale-80 hover:ease-in-out " >
+                            <div class="container mx-auto p-9 bg-white max-w-sm rounded-2xl overflow-hidden shadow-md hover:shadow-2xl transition duration-300">
+                               
+                                <div class="flex flex-col items-center">
                                     <div>
-                                        <h1 class="mt-5 text-2xl font-semibold">{name}</h1>
+                                    <img src={darren} />
+                                        <h1 class="mt-5 text-2xl font-semibold">Status: Submitted</h1>
+                                        <p>Order: Bacon and Egg Taco & Migas & Mojito </p>
+                                        <p></p>
                                     </div>
-                                    <div>
+                                    <div className='pt-5'>
                                         <button class="text-white text-md font-semibold bg-red-400 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">
-                                            Archive Order
+                                            Move to In Progress
                                         </button>
                                     </div>
                                 </div>
